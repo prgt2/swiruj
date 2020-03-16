752ef9a4179d9ded7ebdcc4605f39e87b8e5ade5
prgt2 <excelall@excelall.pl>
2020-03-16 22:10:03 +0100

    resolved evaluation in dom and collecting data about all countries

--- swirus.js
+++ swirus.js
@@ -1,22 +1,49 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-const swirusy = require('./swirusy.json')
+const swirusy = require('./swirusy2.json')
+console.log((swirusy.array))
 async function swiruj() {
     const browser = await puppeteer.launch({
-        //executablePath: "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe",
-        //userDataDir: "mnt/c/Users/User.DESTKOP-AJTIGN3N/AppData/Local/Temp/puppeteer_user_data",
-        //headless: false
+        executablePath: 'snap/bin/chromium'
     })
     const page = await browser.newPage()
-
+    page.setDefaultNavigationTimeout(0); 
     await page.goto('https://www.worldometers.info/coronavirus/')
-    const element_swirusow = await page.$('.content-inner > div:nth-child(7) > div:nth-child(2) > span:nth-child(1)')
-    const liczba_swirusow = await page.evaluate(element_swirusow => element_swirusow.textContent, element_swirusow)
-    let parse_swirus = parseFloat(liczba_swirusow.replace(/,/g, '.'))
+    //await page.goto('file:///home/fit/Pulpit/swiruj/swiruj/worldometers.html')
+    const kolekcja = await page.evaluate(() => {
+        let swirusy = {
+            array: []
+        }
+        let element_swirusow = $('.content-inner > div:nth-child(7) > div:nth-child(2) > span:nth-child(1)').text()
+        let parse_swirusy = element_swirusow.replace(/,/g, '.')
+        let collection_of_texts = []
+        let collection_countries = $('td:nth-child(1)').toArray()
+        let collection_numbers = $('td:nth-child(2)').toArray()
+        let collection_numbers2 = []
+        for (el of collection_numbers) {
+            collection_numbers2.push(($(el).text().replace(/\n|  /g, '').replace(/,/g,'.')))
+        }
+        for ([index, el] of collection_countries.entries()) {
+            collection_of_texts.push((index + 1) + ($(el).text().replace(/\n|  /g, '')) + collection_numbers2[index])
+            
+            let swir = new nowy_rekord(($(el).text().replace(/\n|  /g, '')), collection_numbers2[index])
+            swirusy.array.push(swir)
+        }
+        collection_of_texts.unshift(0 + 'Worldwide' + parse_swirusy)
+        //return collection_of_texts
+
+        function nowy_rekord(country, infected) {
+            this.date = Date()
+            this.country = country
+            this.infected = infected
+        }
+        return swirusy
+
+    })
+    swirusy.array.push(kolekcja)
+    console.log(kolekcja)
+    fs.writeFileSync('./kolekcja', JSON.stringify(kolekcja))
     await browser.close()
-    let swirus = { date: Date(), swirki: parse_swirus }
-    swirusy.array_of_swiry += JSON.stringify(swirus)
-        //console.log(swirusy)
-    fs.writeFile('/.swirusy.json', swirusy, () => console.log(swirusy))
 }
 swiruj()
+
